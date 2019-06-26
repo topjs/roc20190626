9fc4ddf350cb95469c2f06831512fd9ad4df3319
topjs <kevin_smile@msn.com>
2019-06-26 18:23:59 +0800

    create new project

---
+++ src/main/java/cn/utstarcom/RocTest4Application.java
@@ -0,0 +1,13 @@
+package cn.utstarcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocTest4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocTest4Application.class, args);
+    }
+
+}
---
+++ src/main/java/cn/utstarcom/controller/HelloController.java
@@ -0,0 +1,24 @@
+package cn.utstarcom.controller;
+
+import cn.utstarcom.entity.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by UTSC0107 on 2019/6/26.
+ */
+@RestController
+public class HelloController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
